13f62c61dc99f8765acc2423c204a103d2e3f16b
rob1611 <robert.hayrapetyan89@gmail.com>
2023-09-12 12:32:42 +0400

    Initial Commit

---
+++ NAM/NAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAMApp.swift
+//  NAM
+//
+//  Created by Robert Hayrapetyan on 12.09.23.
+//
+
+import SwiftUI
+
+@main
+struct NAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
